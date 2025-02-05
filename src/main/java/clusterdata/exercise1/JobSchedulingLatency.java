1ac205a4e219b715a75ce89f7fa74fb36e8dde60
Owen Mariani <mariani.owen@gmail.com>
2025-02-05 14:49:12 -0500

    completed scheduling latency evaluator

--- src/main/java/clusterdata/exercise1/JobSchedulingLatency.java
+++ src/main/java/clusterdata/exercise1/JobSchedulingLatency.java
@@ -5,6 +5,7 @@ import clusterdata.datatypes.JobEvent;
 import clusterdata.sources.JobEventSource;
 import clusterdata.utils.AppBase;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
@@ -12,8 +13,10 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 import java.sql.Timestamp;
+import java.util.HashMap;
 
 
 /**
@@ -43,7 +46,7 @@ public class JobSchedulingLatency extends AppBase {
                 .addSource(jobSourceOrTest(new JobEventSource(input, servingSpeedFactor)))
                 .setParallelism(1);
 
-        // TODO: process the job events of types `EventType.SCHEDULE` and `EventType.SUBMIT`, measuring
+        // DONE: process the job events of types `EventType.SCHEDULE` and `EventType.SUBMIT`, measuring
         //  the time difference between submission and scheduling.
         // Complete: jobIdWithLatency = ...
 
@@ -54,26 +57,74 @@ public class JobSchedulingLatency extends AppBase {
         KeyedStream<JobEvent, Long> keyedEvents = filteredStream
                 .keyBy(val -> val.jobId);
 
-//        convert to tuple
-        DataStream<Tuple2<Long, Long>> tupleMappedEvents = keyedEvents.map(new EventToTuple());
-
 // Flat Map in order to emit tuple when it is complete
+        DataStream<Tuple2<Long, Long>> flatMappedEvents = keyedEvents.flatMap(new SaveTimestamps());
 
-        printOrTest(tupleMappedEvents);
+        printOrTest(flatMappedEvents);
 
         // execute the dataflow
         env.execute("Job Scheduling Latency");
     }
 
 
-//    Transform Event into empty Tuple for later use
-    public static class EventToTuple implements MapFunction<JobEvent, Tuple2<Long, Long>> {
+//    Flatmap
+    public static class SaveTimestamps implements FlatMapFunction<JobEvent, Tuple2<Long, Long>> {
+    HashMap<Long, Tuple2<Long, Long>> map = new HashMap<>();
+
     @Override
-    public Tuple2<Long, Long> map(JobEvent job) {
-        return Tuple2.of(0L, 0L);
+    public void flatMap(JobEvent job, Collector<Tuple2<Long, Long>> out) throws Exception {
+        // 1) We get a SUBMIT
+        if (job.eventType.equals(EventType.SUBMIT)) {
+            if (map.containsKey(job.jobId)) {
+                Tuple2<Long, Long> storedVal = map.get(job.jobId);
+                // a) We have a SUBMIT already that is later
+                if (storedVal.f0 != -1L && storedVal.f0 > job.timestamp) {
+                    map.put(job.jobId, Tuple2.of(job.timestamp, -1L));
+                }
+                // b) We have a SCHEDULE but not a SUBMIT --> emit
+                else if (storedVal.f0 == -1L && storedVal.f1 != -1L) {
+                    out.collect(Tuple2.of(job.jobId,  storedVal.f1 - job.timestamp));
+                }
+                // c) We do not have a SUBMIT but we have a SCHEDULE
+                else {
+                    storedVal.f0 = job.timestamp;
+                    map.put(job.jobId, storedVal);
+                }
+            }
+            // We don't have either
+            else {
+                map.put(job.jobId, Tuple2.of(job.timestamp, -1L));
+            }
+        }
+
+
+        // 1) We get a SCHEDULE
+        else {
+            if (job.eventType.equals(EventType.SCHEDULE)) {
+                if (map.containsKey(job.jobId)) {
+                    Tuple2<Long, Long> storedVal = map.get(job.jobId);
+                    // a) We have a SCHEDULE already that is earlier
+                    if (storedVal.f1 != -1L && storedVal.f0 < job.timestamp) {
+                        map.put(job.jobId, Tuple2.of(-1L, job.timestamp));
+                    }
+                    // b) We have a SUBMIT but not a SCHEDULE --> emit
+                    else if (storedVal.f1 == -1L && storedVal.f0 != -1L) {
+                        out.collect(Tuple2.of(job.jobId,  job.timestamp - storedVal.f0));
+                    }
+                    // c) We do not have a SCHEDULE but we have a SUBMIT
+                    else {
+                        storedVal.f1 = job.timestamp;
+                        map.put(job.jobId, storedVal);
+                    }
+                }
+                // We don't have either
+                else {
+                    map.put(job.jobId, Tuple2.of(-1L, job.timestamp));
+                }
+            }
+        }
     }
 }
-
 
 //    Only gets job events that are SUBMIT or SCHEDULE
     private static class FilterByEventType implements FilterFunction<JobEvent>{
